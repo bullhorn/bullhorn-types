0efe2e7e7dcb048a0defc774f039236b8383837b
Drew Dudley <drew.dudley@bullhorn.com>
2020-06-12 12:56:13 -0500

    BH-67081 changing id field back to id

--- src/index.ts
+++ src/index.ts
@@ -4580,7 +4580,7 @@ export interface EntryTypeLookup {
     label?: Strings;
 }
 export interface EstaffMappableFlowback {
-    estaffMappableFlowbackID?: number;
+    id?: number;
     estaffFieldType?: Strings;
     estaffRecordType?: Strings;
     atsField?: Strings;
