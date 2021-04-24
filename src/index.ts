25cd5b4cb796b8394a10c7828f8b3a800c3fa36f
sajjansarkar <sajjan.sarkar@gmail.com>
2021-04-24 15:23:21 -0400

    Revert "feat(typings): Adding new field TimeLaborEvalRuleTemplate"
    This reverts commit fa776fa53ba9ac862ad932e6fcc308bf3262cdd4.

--- src/index.ts
+++ src/index.ts
@@ -553,7 +553,6 @@ export class EntityTypes {
     static TimeLaborEvalRuleCategory: 'TimeLaborEvalRuleCategory' = 'TimeLaborEvalRuleCategory';
     static TimeLaborEvalRuleCombineCriteria: 'TimeLaborEvalRuleCombineCriteria' = 'TimeLaborEvalRuleCombineCriteria';
     static TimeLaborEvalRuleStatus: 'TimeLaborEvalRuleStatus' = 'TimeLaborEvalRuleStatus';
-    static TimeLaborEvalRuleTemplate: 'TimeLaborEvalRuleTemplate' = 'TimeLaborEvalRuleTemplate';
     static TimeLaborEvalRuleType: 'TimeLaborEvalRuleType' = 'TimeLaborEvalRuleType';
     static TimeUnit: 'TimeUnit' = 'TimeUnit';
     static TransactionNote: 'TransactionNote' = 'TransactionNote';
@@ -13594,18 +13593,6 @@ export interface TimeLaborEvalRuleCombineCriteria {
 export interface TimeLaborEvalRuleStatus {
     id?: number;
     name?: Strings;
-}
-export interface TimeLaborEvalRuleTemplate {
-    id?: number;
-    addedAtUtc?: Date;
-    addedByUser?: CorporateUser;
-    description?: Strings;
-    isDefault?: boolean;
-    lastModifiedAtUtc?: Date;
-    modifyingUser?: CorporateUser;
-    name?: Strings;
-    timeLaborEvalRules?: ToMany<TimeLaborEvalRule>;
-    timestamp?: Date;
 }
 export interface TimeLaborEvalRuleType {
     id?: number;
