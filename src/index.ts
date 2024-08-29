44307c2dd34bd948e1481d77e9af85a3c45ddd3a
Antony Warring <antony.warring@bullhorn.net>
2024-08-29 07:55:22 -0400

    Adding DirectDepositAccount types

--- src/index.ts
+++ src/index.ts
@@ -236,6 +236,8 @@ export class EntityTypes {
     static DeductionCategoryLookup: 'DeductionCategoryLookup' = 'DeductionCategoryLookup';
     static DefaultEarnCode: 'DefaultEarnCode' = 'DefaultEarnCode';
     static Department: 'Department' = 'Department';
+    static DirectDepositAccount: 'DirectDepositAccount' = 'DirectDepositAccount';
+    static DirectDepositAccountTypeLookup: 'DirectDepositAccountTypeLookup' = 'DirectDepositAccountTypeLookup';
     static Discount: 'Discount' = 'Discount';
     static DiscountEditHistory: 'DiscountEditHistory' = 'DiscountEditHistory';
     static DiscountEditHistoryFieldChange: 'DiscountEditHistoryFieldChange' = 'DiscountEditHistoryFieldChange';
@@ -5077,6 +5079,29 @@ export interface DeliveryMethodLookup {
     label?: Strings;
     modifiedByUser?: CorporateUser;
     shouldShowInPicker?: boolean;
+}
+export interface DirectDepositAccount {
+    id?: number;
+    accountNumber?: string;
+    amount?: number;
+    bankName?: string;
+    candidate?: Candidate;
+    currencyUnit?: CurrencyUnit;
+    dateAdded?: Date;
+    dateLastModified?: Date;
+    deletedByUser?: CorporateUser;
+    directDepositAccountTypeLookup?: DirectDepositAccountTypeLookup;
+    institutionNumber?: string;
+    isDeleted?: boolean;
+    paymentOrder?: number;
+    percentValue?: number;
+    remainder?: boolean;
+    transitNumber?: string;
+}
+export interface DirectDepositAccountTypeLookup {
+    id?: number;
+    isDeleted?: boolean;
+    label?: Strings;
 }
 export interface Discount {
     id?: number;
